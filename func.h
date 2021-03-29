3d4b1623a2eadf43e4c93250ce861bdcea9a0e34
Alie <80550985+emirova@users.noreply.github.com>
2021-03-29 21:21:45 +0300

    Create func.h

---
+++ func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC.H
+#define FUNC
+void swap(int* p1, int* p2);
+
+void qs_sort(int Array[], int N, int start, int end);
+void bubble(int arr[], int size);
+void printarr(int a[], int size);
+
+#endif
